f04e7da81345b361502cf9da45cd7c50efcb5e08
shoxruxshomurodov <shomurodovshoxrux@gmail.com>
2022-08-22 06:21:55 +0500

    feat:NewDocument

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const Styled = styled.input`
+  padding: 15px 25px;
+  border: 1px solid #cdcdcd;
+  border-radius: 1px;
+  display: block;
   width: 100%;
-  padding: 10px 15px;
+  outline: none;
+  transition: 0.2s ease;
+  font-size: 16px;
+  &:focus {
+    border-color: #2dc161;
+    .search-form-btn {
+      border: 1px solid #cdcdcd;
+    }
+  }
 `;
 const Input = ({ ...props }) => {
   return <Styled {...props} />;
